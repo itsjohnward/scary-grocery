0ee40fa9a7a384b7ce5dc707843bce52fa1317ee
John Ward <jsward2@gmail.com>
2016-02-23 04:19:31 -0500

    Generalize sprite class

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 var GAME_SIZE = 2;
 var TILE_SIZE = 16;
+var ROTATIONS = ["left", "right", "up", "down"];
+var player, boogey;
 
 var game = new Phaser.Game(GAME_SIZE*16*16, GAME_SIZE*16*16, Phaser.AUTO, 'content', {
   preload: preload,
@@ -8,126 +10,66 @@ var game = new Phaser.Game(GAME_SIZE*16*16, GAME_SIZE*16*16, Phaser.AUTO, 'conte
   render: render
 });
 
-var player = {
-	name: "player",
-	url: "./assets/player.png",
-	moveTime: 0,
-	moveThreshold: 250,
-	x: 2,
-	y: 2,
-	moveLeft: function() {
-		console.log("x: " + this.x + " / y: " + this.y);
-		if(!(tiles[this.y][this.x-1].collideable)){
-			console.log("moveLeft");
-			if((this.moveTime + this.moveThreshold) < game.time.now) {
-				this.x--;
+function Sprite(name, url, x, y) {
+	this.move = function(direction) {
+		if((this.moveTime + this.moveThreshold) < game.time.now) {
+			if(ROTATIONS[this.rotation] != direction) {
+				var direction_int = ROTATIONS.indexOf(direction);
+				this.rotation = direction_int >= 0 ? direction_int : this.rotation;
 				this.moveTime = game.time.now;
-				console.log("x: " + this.x + " / y: " + this.y);
 			}
 			else {
-				console.log("move: " + this.moveTime+this.moveThreshold + " / game: " + game.time.now);
+				var x_par = 0;
+				var y_par = 0;
+				if(direction == "left") {
+					x_par = -1;
+				} else if (direction == "right") {
+					x_par = 1;
+				} else if (direction == "up") {
+					y_par = -1;
+				} else if (direction == "down") {
+					y_par = 1;
+				} else {
+					return;
+				}
+				if(!(tiles[this.y + y_par][this.x + x_par].collideable)){
+					this.x += x_par;
+					this.y += y_par;
+					this.moveTime = game.time.now;
+				}
+				else {
+					//COLLIDE
+				}
 			}
+			this.updateLocation();
 		}
-		else {
-			console.log("can't moveLeft");
-			console.log(tiles[this.y][this.x-1]);
-			// Sound Effect
-		}
-		this.updateLocation();
-	},
-	moveRight: function() {
-		console.log("x: " + this.x + " / y: " + this.y);
-		if(!(tiles[this.y][this.x+1].collideable)){
-			console.log("moveRight");
-			if((this.moveTime + this.moveThreshold) < game.time.now) {
-				this.x++;
-				this.moveTime = game.time.now;
-				console.log("x: " + this.x + " / y: " + this.y);
-			}
-			else {
-				console.log("move: " + this.moveTime+this.moveThreshold + " / game: " + game.time.now);
-			}
-		}
-		else {
-			console.log("can't moveRight");
-			console.log(tiles[this.y][this.x+1]);
-			// Sound Effect
-		}
-		this.updateLocation();
-	},
-	moveUp: function() {
-		console.log("x: " + this.x + " / y: " + this.y);
-		if(!(tiles[this.y-1][this.x].collideable)){
-			console.log("moveUp");
-			if((this.moveTime + this.moveThreshold) < game.time.now) {
-				this.y--;
-				this.moveTime = game.time.now;
-				console.log("x: " + this.x + " / y: " + this.y);
-			}
-			else {
-				console.log("move: " + this.moveTime+this.moveThreshold + " / game: " + game.time.now);
-			}
-		}
-		else {
-			console.log("can't moveUp");
-			console.log(tiles[this.y-1][this.x]);
-			// Sound Effect
-		}
-		this.updateLocation();
-	},
-	moveDown: function() {
-		console.log("x: " + this.x + " / y: " + this.y);
-		if(!(tiles[this.y+1][this.x].collideable)){
-			console.log("moveDown");
-			if((this.moveTime + this.moveThreshold) < game.time.now) {
-				this.y++;
-				this.moveTime = game.time.now;
-				console.log("x: " + this.x + " / y: " + this.y);
-			}
-			else {
-				console.log("move: " + this.moveTime+this.moveThreshold + " / game: " + game.time.now);
-			}
-		}
-		else {
-			console.log("can't moveDown");
-			console.log(tiles[this.y+1][this.x]);
-			// Sound Effect
-		}
-		this.updateLocation();
-	},
-	updateLocation: function() {
-		this.sprite.x = this.getX();
-		this.sprite.y = this.getY();
-	},
-	getX: function() {
-		return this.x * GAME_SIZE * TILE_SIZE;
-	},
-	getY: function() {
-		return this.y * GAME_SIZE * TILE_SIZE;
 	}
-};
-var boogey = {
-	name: "boogey",
-	url: "./assets/boogey.png",
-	moveTime: 0,
-	moveThreshold: 250,
-	x: 11,
-	y: 6,
-	moveLeft: function() {
-
-	},
-	moveRight: function() {
-
-	},
-	moveUp: function() {
-
-	},
-	moveDown: function() {
-
+	this.updateLocation = function() {
+		this.sprite.x = this.getX(); // ADD LERP
+		this.sprite.y = this.getY(); // ADD LERP
+		//console.log(this.sprite.scale.x);
+		this.sprite.scale.x = Math.abs(this.sprite.scale.x) - (this.rotation % 2) * 2 * Math.abs(this.sprite.scale.x);
 	}
-};
+	this.getX = function() {
+		return this.x * GAME_SIZE * TILE_SIZE + TILE_SIZE/2;
+	}
+	this.getY = function() {
+		return this.y * GAME_SIZE * TILE_SIZE + TILE_SIZE/2;
+	}
 
-var buttonTime = 0;
+	this.name = name;
+	this.url = url;
+	this.x = x;
+	this.y = y;
+	this.sprite = game.add.sprite(this.getX(), this.getY(), this.name);
+	this.sprite.width = TILE_SIZE * GAME_SIZE;
+	this.sprite.height = TILE_SIZE * GAME_SIZE;
+	this.sprite.anchor.setTo(0.5, 0.5);
+	this.moveTime = 0;
+	this.moveThreshold = 250;
+	this.rotation = 0;
+
+}
 
 function Tile(val, x, y, height, width, collideable) {
   this.url = tileset[val].url;
@@ -232,8 +174,8 @@ function preload() {
 		game.load.image(tileset[i].name, tileset[i].url);
 	}
 	game.load.image(tileset[i].name, tileset[i].url);
-	game.load.spritesheet(player.name, player.url, 32, 48);
-	game.load.spritesheet(boogey.name, boogey.url, 32, 32);
+	game.load.spritesheet("player", "./assets/player.png", 32, 48);
+	game.load.spritesheet("boogey", "./assets/boogey.png", 32, 32);
 }
 
 function create() {
@@ -251,37 +193,24 @@ function create() {
 		y += GAME_SIZE * TILE_SIZE;
 	}
 
-
-	/*
-
-		v UPDATE LOCATION PARAMETERS
-			Since x and y were added to player object
-
-	*/
-
-	player.sprite = game.add.sprite(player.getX(), player.getY(), player.name);
-	console.log(player.sprite.width);
-	player.sprite.width = TILE_SIZE * GAME_SIZE;
-	player.sprite.height = TILE_SIZE * GAME_SIZE;
-	console.log(player.sprite.height);
-	boogey.sprite = game.add.sprite(tiles[11][6].x, tiles[11][6].y, boogey.name);
-	boogey.sprite.width = TILE_SIZE * GAME_SIZE;
-	boogey.sprite.height = TILE_SIZE * GAME_SIZE;
+	player = new Sprite("player", "./assets/player.png", 2, 2);
+	boogey = new Sprite("boogey", "./assets/boogey.png", 11, 6);
+	//console.log(player.sprite.height);
 }
 
 function update() {
 
 	if (game.input.keyboard.isDown(Phaser.Keyboard.W)) {
-		player.moveUp();
+		player.move("up");
 	}
 	else if (game.input.keyboard.isDown(Phaser.Keyboard.A)) {
-		player.moveLeft();
+		player.move("left");
 	}
 	else if (game.input.keyboard.isDown(Phaser.Keyboard.S)) {
-		player.moveDown();
+		player.move("down");
 	}
 	else if (game.input.keyboard.isDown(Phaser.Keyboard.D)) {
-		player.moveRight();
+		player.move("right");
 	}
 
 
